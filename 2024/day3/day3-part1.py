0dc6dc7fcd18e78e393d06cd466ac9c00ec89c65
Michael Scuteri <michael@Michaels-MacBook-Pro.local>
2024-12-06 00:26:27 +1100

    day3 part one completed

---
+++ 2024/day3/day3-part1.py
@@ -0,0 +1,13 @@
+import re
+
+with open('day3.txt', 'r') as file: content = file.read()
+pattern = r"mul\((\d+),(\d+)\)"
+matches = re.findall(pattern, content)
+
+totals = []
+
+for item in matches:
+    total = int(item[0]) * int(item[1])
+    totals.append(total)
+
+print(sum(totals))
